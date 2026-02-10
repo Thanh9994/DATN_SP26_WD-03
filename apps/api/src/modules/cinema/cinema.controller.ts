b28dc94e256727e13b49762e5ee423e8f1989226
ThanhTran <you@example.com>
2026-02-10 22:37:58 +0700

    Thêm Update và xóa Rạp chiếu phim (Cinema)

--- apps/api/src/modules/cinema/cinema.controller.ts
+++ apps/api/src/modules/cinema/cinema.controller.ts
@@ -13,6 +13,22 @@ export const AllCinemas = async (req: Request, res: Response) => {
     }
 };
 
+export const getCinemaById = async (req: Request, res: Response) => {
+    try {
+        const {id} = req.params;
+        const cinema = await cinemaModel.findById(id);
+        if(!cinema) {
+            return res.status(404).json({ message: "Rạp không tồn tại" });
+        }
+        res.status(200).json({
+            message: "Lấy thông tin rạp thành công",
+            data: cinema
+        });
+    } catch (error) {
+        res.status(500).json({ message: error});
+    }
+};
+
 export const createCinema = async (req: Request, res: Response) => {
     try {
         const newCinema = await cinemaModel.create(req.body);
@@ -20,8 +36,41 @@ export const createCinema = async (req: Request, res: Response) => {
             message: "Tạo rạp mới thành công",
             data: newCinema
         });
-        res.json(createCinema)
     } catch (error) {
         res.status(400).json({ message: error});
     }
 };
+
+export const updateCinema = async (req: Request, res: Response) => {
+    try {
+        const {id} = req.params;
+        const updateData = req.body;
+        const updatedCinema = await cinemaModel.findByIdAndUpdate(
+            id, 
+            { ...updateData, $inc: { views: 1 }, }, 
+            { new: true}
+        );
+        if(!updatedCinema) {
+            return res.status(404).json({ message: "Rạp không tồn tại" });
+        };
+        res.status(200).json({
+            message: "Cập nhật rạp thành công",
+            data: updatedCinema
+        });
+    } catch (error) {
+        res.status(400).json({ message: error});
+    }
+};
+
+export const deleteCinema = async (req: Request, res: Response) => {
+    try {
+        const {id} = req.params;
+        await cinemaModel.findByIdAndDelete(id);
+        res.status(200).json({
+            message: "Xóa rạp thành công",
+        });
+    } catch (error) {
+        res.status(400).json({ message: error});
+    }
+
+}
