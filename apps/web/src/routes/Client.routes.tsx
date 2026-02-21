d6a73330718cb22d318e62f163b45f0eb285b4e9
xson0209 <xson0209@gmail.com>
2026-02-21 21:25:16 +0700

    fix: correct footer structure and closing tags

--- apps/web/src/routes/Client.routes.tsx
+++ apps/web/src/routes/Client.routes.tsx
@@ -1,13 +1,15 @@
 import { NotFound } from '@web/components/NotFound'
 import { ClientLayout } from '@web/layouts/ClientLayout'
 import { Home } from '@web/pages/Home'
+import Login from '@web/layouts/client/Login'
 import { RouteObject } from 'react-router-dom'
 
 export const ClientRoutes: RouteObject = {
-    path: '/',
-    element: <ClientLayout/>,
-    children: [
-        { path: '', element: <Home />},
-        { path: '*', element: <NotFound />}
-    ]
+  path: '/',
+  element: <ClientLayout />,
+  children: [
+    { index: true, element: <Home /> },     
+    { path: 'login', element: <Login /> }, 
+    { path: '*', element: <NotFound /> }
+  ]
 }
