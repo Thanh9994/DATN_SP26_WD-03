f45bd5f4ad4031d2f50122781756511e00bc2fc4
xson0209 <xson0209@gmail.com>
2026-02-21 21:25:16 +0700

    fix: correct footer structure and closing tags

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import './index.css'
-import App from './App.tsx'
+import './index.css';
+import App from './App.tsx';
 import "antd/dist/reset.css"; 
 
 const queryClient = new QueryClient();
