bdd097600bea137f7a502d36973762c75e93e99b
Thanh Tran <tthanhs678@gmail.com>
2026-02-23 22:19:16 +0700

    Xson0209 #5
    * header components
    
    * fix: correct footer structure and closing tags
    
    * fix: correct footer structure and closing tags
    
    * feat: update header, footer, register page and client routes
    
    * feat: add register UI and update client routes
    
    * chore: remove nested DATN_SP26_WD-03 folder
    
    ---------
    
    Co-authored-by: xson0209 <xson0209@gmail.com>

--- apps/web/src/routes/Client.routes.tsx
+++ apps/web/src/routes/Client.routes.tsx
@@ -1,13 +1,17 @@
 import { NotFound } from '@web/components/NotFound'
 import { ClientLayout } from '@web/layouts/ClientLayout'
 import { Home } from '@web/pages/Home'
+import Login from '@web/layouts/client/Login'
 import { RouteObject } from 'react-router-dom'
+import Register from '@web/layouts/client/Register'
 
 export const ClientRoutes: RouteObject = {
-    path: '/',
-    element: <ClientLayout/>,
-    children: [
-        { path: '', element: <Home />},
-        { path: '*', element: <NotFound />}
-    ]
-}
+  path: '/',
+  element: <ClientLayout />,
+  children: [
+    { index: true, element: <Home /> },     
+    { path: 'login', element: <Login /> }, 
+    { path: '*', element: <NotFound /> },
+    { path: 'register', element: <Register /> },
+  ]
+} 
