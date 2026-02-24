fbd43a99c452d12a98913023ec336456bec9a920
ThanhTran <you@example.com>
2026-02-24 16:27:06 +0700

    Thêm tính năng tải lên ảnh với Cloudinary, cải thiện giao diện và cập nhật định tuyến cho trang quản trị

--- apps/web/src/routes/Admin.routes.tsx
+++ apps/web/src/routes/Admin.routes.tsx
@@ -1,13 +1,13 @@
-import { Upload } from "@web/pages/admin/Upload";
 import { AdminLayouts } from "@web/layouts/AdminLayout";
 import { RouteObject } from "react-router-dom";
 import { Dashboard } from "@web/pages/admin/Dashboard";
+import { Upload } from "@web/pages/admin/Upload";
 
 export const AdminRoutes: RouteObject = {
   path: "/admin",
   element: <AdminLayouts />,
   children: [
     { index: true, element: <Dashboard /> },
-    { path: "upload", element: <Upload /> },
+    { path: "media", element: <Upload/> },
   ],
 };
