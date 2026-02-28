2dbb1a756c3529e105f143f641e0828958dafd61
ThanhTran <you@example.com>
2026-02-28 18:06:37 +0700

    siawr

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import cors from "cors";
 import uploadRouter from "./middlewares/upload";
-import productRouter from "./modules/sales-operations/products/product.route";
 import catalogRouter from "./modules/cinema-catalog";
 import accessRouter from "./modules/access-control";
 import contentRouter from "./modules/movie-content";
 import testRoute from "./modules/test/test.route";
+import oderRouter from "./modules/sales-operations";
 
 const app = express();
 
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 // Routes
 app.use("/api/test", testRoute);
 app.use("/api/uploads", uploadRouter);
-app.use("/api/product", productRouter);
+app.use("/api/oder", oderRouter);
 app.use("/api/access", accessRouter);
 app.use("/api/catalog", catalogRouter);
 app.use("/api/content", contentRouter);
--- apps/api/src/modules/sales-operations/index.ts
+++ apps/api/src/modules/sales-operations/index.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
+import productRouter from "./products/product.route";
 
 const oderRouter = Router()
 
-oderRouter.use("");
-oderRouter.use("");
-oderRouter.use("");
-oderRouter.use("");
+oderRouter.use("/product", productRouter);
+// oderRouter.use("");
+// oderRouter.use("");
+// oderRouter.use("");
 
 export default oderRouter;
