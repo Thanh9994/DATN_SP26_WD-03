328a9ce2849073959255c717a0942964fe2a0291
xson0209 <xson0209@gmail.com>
2026-03-10 20:12:26 +0700

    update

--- apps/web/src/routes/Admin.routes.tsx
+++ apps/web/src/routes/Admin.routes.tsx
@@ -1,4 +1,4 @@
-import { AdminLayouts } from "@web/layouts/AdminLayout";
+
 import { RouteObject } from "react-router-dom";
 import { Dashboard } from "@web/pages/admin/Dashboard";
 import { Upload } from "@web/pages/admin/access-control/Upload";
@@ -11,6 +11,7 @@ import Product from "@web/pages/admin/Product";
 import { Rooms } from "@web/pages/admin/Rooms";
 import Promotion from "@web/pages/admin/promotion/Promotion";
 import PromotionForm from "@web/pages/admin/promotion/PromotionForm";
+import { AdminLayouts } from "@web/layouts/AdminLayout";
 
 export const AdminRoutes: RouteObject = {
   path: "/admin",
