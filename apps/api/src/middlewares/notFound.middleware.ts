919a84c1b2c2bcf5ae2c4f4d3ac9c380879cf3a1
ThanhTran <you@example.com>
2026-03-13 22:44:58 +0700

    ád

--- apps/api/src/middlewares/notFound.middleware.ts
+++ apps/api/src/middlewares/notFound.middleware.ts
@@ -1,12 +1,15 @@
 import { Request, Response, NextFunction } from "express";
+import { AppError } from "./error.middleware";
 
 export const notFoundMiddleware = (
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
-  res.status(404).json({
-    success: false,
-    message: `Route ${req.originalUrl} không tồn tại`,
-  });
+  next(
+    new AppError(
+      `Đường dẫn ${req.originalUrl} không tồn tại trên hệ thống`,
+      404,
+    ),
+  );
 };
