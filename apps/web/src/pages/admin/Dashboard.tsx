5b375c1c038f4d16bb7b6e6d28a419256df72910
Thanh Tran <tthanhs678@gmail.com>
2026-03-16 22:28:45 +0700

    Hoanganh (#104)
    * asdas
    
    * áas
    
    * sua loi seatbooking
    
    * sua loi seatbooking
    
    * showtime
    
    * checkout
    
    * fix seatbooking
    
    * them router
    
    * cinemadetail
    
    * feat: them giao dien cinema
    
    * them giao dien cinemadetail
    
    * them router detail
    
    * fix router
    
    * fix loi
    
    * fix
    
    * Implement email verification process during registration and add verification route
    
    * update register
    
    * refactor(auth): update registration process and email verification flow
    
    * feat(auth): implement OTP verification and resend functionality
    
    * sua giao dien mobile
    
    * sua loi mobile detail
    
    ---------
    
    Co-authored-by: hoanganh <anhnhph53204@gmail.com>

--- apps/web/src/pages/admin/Dashboard.tsx
+++ apps/web/src/pages/admin/Dashboard.tsx
@@ -1,9 +1,13 @@
-import { ShowTimeDashboard } from './dashboard/ShowtimeDashboard';
+import { ShowTimeDashboard } from "./dashboard/ShowtimeDashboard";
 
 export const Dashboard = () => {
   return (
     <div className="space-y-4">
       <ShowTimeDashboard />
     </div>
-  );
-};
+);
+}
+
+
+
+
