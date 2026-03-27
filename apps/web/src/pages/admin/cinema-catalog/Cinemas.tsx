078e6bc0c81fb1c028d9819863f97c8e771480af
hoanganh <anhnhph53204@gmail.com>
2026-03-27 21:16:49 +0700

    fix: correct import statement for Cinemas component

--- apps/web/src/pages/admin/cinema-catalog/Cinemas.tsx
+++ apps/web/src/pages/admin/cinema-catalog/Cinemas.tsx
@@ -16,7 +16,7 @@ import {
 } from 'antd';
 import { useCinemas } from '@web/hooks/useCinema';
 import { ICinema, IPhong } from '@shared/schemas';
-import Cinemas from '@web/pages/Cinemas';
+import  Cinemas from '@web/pages/Cinemas';
 
 const { Title } = Typography;
 
