03b6954196a135921cca285b510830309e622ea2
xson0209 <xson0209@gmail.com>
2026-04-07 19:12:13 +0700

    router + layout header staff

--- apps/web/src/layouts/StaffLayout.tsx
+++ apps/web/src/layouts/StaffLayout.tsx
@@ -3,9 +3,11 @@ import { StaffHeader } from './staff/StaffHeader';
 
 export const StaffLayout = () => {
   return (
-    <div className="flex min-h-screen flex-col bg-background-dark font-display">
+    <div className="flex min-h-screen flex-col bg-background-dark font-display text-white">
       <StaffHeader />
-      <Outlet />
+      <main className="flex-1 px-6 py-4">
+        <Outlet />
+      </main>
     </div>
   );
 };
