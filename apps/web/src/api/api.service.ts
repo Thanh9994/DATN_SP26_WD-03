dc714da8ee224a699c403846302e5b7512b7b4c3
xson0209 <150009185+xson0209@users.noreply.github.com>
2026-04-15 17:36:13 +0700

    call api + useHook dashboard staff (#195)

--- apps/web/src/api/api.service.ts
+++ apps/web/src/api/api.service.ts
@@ -21,6 +21,9 @@ export const API = {
   STAFF: `${API_URL}/staff`,
   CHECKIN_TICKET_WARNING: `${API_URL}/staff/checkin-ticket-warning`,
   STAFF_SHOWTIME_ALERTS: `${API_URL}/staff/showtime-alerts`,
+  STAFF_DASHBOARD_OVERVIEW: `${API_URL}/staff/dashboard/overview`,
+  STAFF_DASHBOARD_UPCOMING_SHOWS: `${API_URL}/staff/dashboard/upcoming-shows`,
+  STAFF_DASHBOARD_RECENT_CHECKINS: `${API_URL}/staff/dashboard/recent-checkins`,
 
   PAYMENT_GATEWAY: `http://localhost:5000/payments`,
 };
